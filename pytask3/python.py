5977fb7bf443b42d09f459982d8a8979352747a7
Rauf <71344074+Raoof-111@users.noreply.github.com>
2024-05-07 17:20:55 +0400

    Add files via upload

---
+++ pytask3/python.py
@@ -0,0 +1,19 @@
+
+
+
+
+class cars():
+
+    def __init__(self, name, il, reng):
+        self.name = name
+        self.il = il
+        self.reng = reng
+        
+      
+BMW = cars("BMW", 2014, "Qara")
+Audi = cars("Audi", 2005, "Ağ")
+
+
+print(f"{BMW.name} {BMW.il} il və {BMW.reng}-dır. ")
+print(f"{Audi.name} {Audi.il} il və {Audi.reng}-dır. ")
+
